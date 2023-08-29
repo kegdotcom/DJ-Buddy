d81da355f8de3a5e3ade72eafc789fe32dae0d88
Keg Avakian <kjavakian@outlook.com>
2023-08-29 13:29:06 -0500

    added some more information to some pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -3,8 +3,11 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <h1>About</h1>
-      <p>about text</p>
+      <h1>About DJ Buddy</h1>
+      <h5>
+        DJ Buddy is a React application that uses the OpenAI Response API and
+        the Spotify Web API in order to get you music recommendations!
+      </h5>
     </>
   );
 }
